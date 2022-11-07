3188cf4fec3b73774fe37061e1a2dc688b8bb6ba
audrey curbera <curbera.audrey@gmail.com>
2022-11-07 11:43:30 +0100

    creation des fichiers javascript

---
+++ front/js/script.js
@@ -0,0 +1,18 @@
+//creation de <a>
+let linkProduct = document.createElement("a")
+
+//creation de <article>
+
+let articleProduct = document.createElement("article")
+
+//creation de <img>
+let imgProduct = document.createElement("img")
+
+//creation de <h3>
+
+let titreProduct = document.createElement("h3")
+
+//creation de <p>
+
+let pProduct = document.createElement("p")
+
