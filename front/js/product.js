f54a5fe5a9004150a5c2bb5aa9b1a93b28b75eef
audrey curbera <curbera.audrey@gmail.com>
2022-11-16 09:54:53 +0100

    product partie 4finie

--- front/js/product.js
+++ front/js/product.js
@@ -4,15 +4,6 @@ var urlparams = new URLSearchParams(url.search);
 var productId = urlparams.get("id")
 console.log(productId);
 
-
-//https://www.youtube.com/watch?v=li7Fmtk4RCo
-//let params = new URLSearchParams(window.location.search);
-
-//URL de base 
-
-
-//fetch(" http://localhost:3000/api/products")   //type de requete GET 
-
 let newurl = "http://localhost:3000/api/products/" + productId ; 
 
 fetch(newurl)
@@ -62,3 +53,84 @@ fetch(newurl)
     console.log(err) ;
     // Une erreur est survenue
   });
+
+
+
+
+
+
+//const elementClick = document.getElementById("addToCard") ;
+//elementClick.addEventListener ("click",function(event){
+
+//})
+
+
+
+
+  //création de la fonction pour sauvegarder le produit dans un panier ! 
+
+  function saveCanap (canap ) {  //on enregistre le tableau dans le local storage
+    localStorage.setItem("canap", JSON.stringify(canap)) ;   // transforme en chaine de caractère
+  }
+
+  function getCanap() {
+    let canap = localStorage.getItem("canap")
+    if (canap == null) {
+      return []; //un tableau vide
+    } else {
+      return JSON.parse (canap);  //parse retransforme la chaine de caractère en objet 
+    }
+  }
+
+
+  //ajout dans le panier
+
+  function addCanap (product) {
+    let canap = getCanap () ;
+    //gerer la quantité
+    let foundProduct = canap.find(p => p.id == product.id == color.value);  //je cherche dans mon panier si il y a un id qui =  l'id du product et = color
+    if (foundProduct != undefined) {
+      foundProduct.quantity++ ;
+    }
+    else {
+      product.quantity = 1 ;
+      canap.push(product);
+    }
+    
+    saveCanap(canap) ;
+  }
+
+
+ //retirer un produit du panier 
+ 
+ function removeCanap (product){
+  let canap = getCanap (); // on recupere
+  canap = canap.filter (p => p.id != product.id); 
+  saveCanap(canap) ;
+}
+
+
+//retirer de la quantité
+
+function changeQuantity (product,quantity) {
+  let canap = getCanap () ; 
+  let foundProduct = canap.find(p => p.id == product.id);  //je cherche dans mon panier si il y a un id qui = a l'id du product
+    if (foundProduct != undefined) {
+      foundProduct.quantity += quantity ;
+      if(foundProduct.quantity <=0 ){   //pour pas avoir une quantité negative
+        removeCanap (foundProduct);
+      } else{
+        saveCanap(canap);
+      }
+    }
+    
+  }
+//pour calcul de la quantité 
+
+//function getNumberProduct {
+  //let canap = getCanap () ;  // on recupere le panier 
+
+//}
+//pour le prix
+
+//function getTotalPrice
